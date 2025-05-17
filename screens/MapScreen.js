7b6a79c8ea2f2a82f7b690a806184d573fc99823
sandeepkishor <sandeepkishore229@gmail.com>
2025-05-17 17:59:45 +0530

    added safeareaview

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+
 
 const MapScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
+      <View>
       <Text>here is the map screen</Text>
     </View>
+    </SafeAreaView>
+    
   )
 }
 
